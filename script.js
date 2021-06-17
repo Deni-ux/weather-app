97662c4f6cf7d6ac80c8fa84f59d333c3fdd9254
Denisa Haka <denisa.haka@ymail.com>
2021-06-17 11:37:58 +0100

    added current weather conditions

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function formatDate(date){
 let currentDate=document.querySelector("#displayDate");
 currentDate.innerHTML=formatDate(nowDate);
 
+
+function showWeather(response) {
+  document.querySelector("#user-city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weatherDescription").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#temp-min").innerHTML = `${Math.round(
+    response.data.main.temp_min
+  )}° `;
+  document.querySelector("#temp-max").innerHTML = `${Math.round(
+    response.data.main.temp_max
+  )}°`;
+
+  let weatherIcon = document.querySelector("#weather-icon");
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
